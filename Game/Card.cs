8806d1b96cc467207e81910a3bc33dba4078ec29
Jakedez <jakedez7@gmail.com>
2022-11-02 09:31:35 -0600

    created "selectCard()" method and "switchCard()" method

--- Game/Card.cs
+++ Game/Card.cs
@@ -1,10 +1,23 @@
+using System;
+using System.Collections.Generic;
+
 namespace CSE210_02.Game
 {
     public class Card
     {
         //ATTRIBUTES
-
+            public int lastCard;
+            public int currentCard;
         //CONSTRUCTOR
+            public void selectCard(){
+                var random = new Random();
+                currentCard = (random.Next() % 13) + 1;
+                return;
+            }
+            public void switchCard(){
+                lastCard = currentCard;
+                return;
+            }
         public Card()
         {
 
